a78b2d13d75171bf0f119d7c23ea2689daf4371f
Zaid Naseer <naseerzaid@gmail.com>
2024-09-11 10:56:56 +0530

    external js

---
+++ scrollbar.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const progressBar = document.getElementById('progressBar');
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    progressBar.style.width = scrollPercent + '%';
+    
+    
+});
