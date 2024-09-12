ad8d0f47e2fafbadfd55a9fe0f63746b6dbe926a
AnasFaaiz <2310030087@klh.edu.in>
2024-09-12 12:13:26 +0530

    card animations

---
+++ animation.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll('.card');
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+          observer.unobserve(entry.target); // Stop observing once the animation is triggered
+        }
+      });
+    }, {
+      threshold: 0.1 // Adjust this value as needed
+    });
+  
+    cards.forEach(card => {
+      observer.observe(card);
+    });
+  });
+  
