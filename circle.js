33906c0ff4db259e5ce6f55bf6c2f8898d50c06e
Zaid Naseer <naseerzaid@gmail.com>
2024-09-12 12:00:16 +0530

    separated circle js

---
+++ circle.js
@@ -0,0 +1,15 @@
+const el = document.getElementById("circle")
+var curOverflow = el.style.overflow;
+
+if ( !curOverflow || curOverflow === "visible" )
+    el.style.overflow = "hidden";
+
+var isOverflowing = el.clientWidth < el.scrollWidth 
+    || el.clientHeight < el.scrollHeight;
+
+el.style.overflow = curOverflow;
+
+if (isOverflowing){
+    console.log("Placing the circle in the middle of the image as the text is overflowing.");
+}
+
